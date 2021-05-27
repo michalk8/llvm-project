264a97c83d533473b9cd145ed8448b32dabcebfd
Michal Klein <michal.klein@protonmail.com>
2021-05-27 15:11:36 +0200

    Add first version of AA dropping

--- llvm/include/llvm/Transforms/Utils/DropAA.h
+++ llvm/include/llvm/Transforms/Utils/DropAA.h
@@ -1,7 +1,12 @@
 #ifndef LLVM_TRANSFORMS_DROPAA_H
 #define LLVM_TRANSFORMS_DROPAA_H
 
+#include <random>
+#include <string>
+#include <unordered_map>
+#include "llvm/Transforms/IPO/Attributor.h"
 #include "llvm/IR/PassManager.h"
+#include "llvm/ADT/SmallVector.h"
 
 namespace llvm {
 
@@ -9,6 +14,31 @@ class DropAAPass: public PassInfoMixin<DropAAPass> {
 public:
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
 };
+
+class SmallMap {
+public:
+  using StorageTy = SmallVector<float, 6>;
+  using ValueTy = StorageTy::value_type;
+
+  SmallMap(ValueTy Fn, ValueTy FnArg, ValueTy FnRet,
+           ValueTy Cb, ValueTy CbArg, ValueTy CbRet) {
+    Storage.resize(6);
+    Storage[0] = clamp(Fn); Storage[1] = clamp(FnArg); Storage[2] = clamp(FnRet);
+    Storage[3] = clamp(Cb); Storage[4] = clamp(CbArg); Storage[5] = clamp(CbRet);
+  }
+
+  ValueTy operator[](IRPosition::Kind) const;
+  static ValueTy clamp(ValueTy Val) {
+    return std::max(ValueTy(0), std::min(Val, ValueTy(1)));
+  };
+private:
+  StorageTy Storage;
+};
+
+bool removeAttributes(IRPosition&,
+                      const SmallMap&,
+                      std::mt19937 &,
+                      std::uniform_real_distribution<SmallMap::ValueTy>&);
 
 } // namespace llvm
 
--- llvm/lib/Transforms/Utils/DropAA.cpp
+++ llvm/lib/Transforms/Utils/DropAA.cpp
@@ -1,11 +1,97 @@
+#include <sstream>
+#include <random>
 #include "llvm/Transforms/Utils/DropAA.h"
-#include "llvm/Transforms/IPO/Attributor.h"
 
 using namespace llvm;
 
+static cl::opt<unsigned>
+    RandomSeed("dropaa-seed", cl::Hidden, cl::desc("Random seed."), cl::init(0));
+static cl::opt<float>
+    FnKeep("dropaa-fn-keep", cl::Hidden, cl::desc("f"), cl::init(1.0));
+static cl::opt<float>
+    FnArgKeep("dropaa-fna-keep", cl::Hidden, cl::desc("f"), cl::init(1.0));
+static cl::opt<float>
+    FnRetKeep("dropaa-fnr-keep", cl::Hidden, cl::desc("f"), cl::init(1.0));
+static cl::opt<float>
+    CbKeep("dropaa-cb-keep", cl::Hidden, cl::desc("f"), cl::init(1.0));
+static cl::opt<float>
+    CbArgKeep("dropaa-cba-keep", cl::Hidden, cl::desc("f"), cl::init(1.0));
+static cl::opt<float>
+    CbRetKeep("dropaa-cbr-keep", cl::Hidden, cl::desc("f"), cl::init(1.0));
+
+
+SmallMap::ValueTy SmallMap::operator[](IRPosition::Kind kind) const {
+  switch (kind) {
+  case IRPosition::Kind::IRP_FUNCTION:
+    return Storage[0];
+  case IRPosition::Kind::IRP_ARGUMENT:
+    return Storage[1];
+  case IRPosition::Kind::IRP_RETURNED:
+    return Storage[2];
+  case IRPosition::Kind::IRP_CALL_SITE:
+    return Storage[3];
+  case IRPosition::Kind::IRP_CALL_SITE_ARGUMENT:
+    return Storage[4];
+  case IRPosition::Kind::IRP_CALL_SITE_RETURNED:
+    return Storage[5];
+  default:
+    llvm_unreachable("Invalid IR position kind!");
+  }
+}
+
+bool llvm::removeAttributes(IRPosition& IRP,
+                            const SmallMap& FracToKeep,
+                            std::mt19937& Mt,
+                            std::uniform_real_distribution<SmallMap::ValueTy>& Rng) {
+  AttributeList AttrList, NewAttrList;
+  SmallMap::ValueTy Threshold = FracToKeep[IRP.getPositionKind()];
+  bool changed = false;
+
+  auto *CB = dyn_cast<CallBase>(&IRP.getAnchorValue());
+  if (CB)
+    AttrList = CB->getAttributes();
+  else
+    AttrList = IRP.getAssociatedFunction()->getAttributes();
+  NewAttrList = AttrList;
+
+  LLVMContext &Ctx = IRP.getAnchorValue().getContext();
+  // TODO: verify it's correct
+  for (auto &AS: AttrList) {
+    for (auto &A: AS) {
+      if (Rng(Mt) > Threshold) {
+        NewAttrList = NewAttrList.removeAttribute(Ctx, IRP.getAttrIdx(), A.getKindAsEnum());
+        changed = true;
+      }
+    }
+  }
+
+  if (CB)
+    CB->setAttributes(NewAttrList);
+  else
+    IRP.getAssociatedFunction()->setAttributes(NewAttrList);
+
+  return changed;
+}
+
 PreservedAnalyses DropAAPass::run(Function &F,
                                   FunctionAnalysisManager &AM) {
-  errs() << "Function name: " << F.getName() << "\n";
+  SmallMap FracToKeep = SmallMap(FnKeep.getValue(), FnArgKeep.getValue(), FnRetKeep.getValue(),
+                                 CbKeep.getValue(), CbArgKeep.getValue(), CbRetKeep.getValue());
+
+  std::mt19937 Mt(RandomSeed.getValue());
+  std::uniform_real_distribution<SmallMap::ValueTy> Rng(0.0, 1.0);
+  bool changed = false;
+
+  IRPosition IRP = IRPosition::function(F);
+  changed |= removeAttributes(IRP, FracToKeep, Mt, Rng);
+
+  for (Argument& Arg: F.args()) {
+    IRPosition ArgPos = IRPosition::argument(Arg);
+    changed |= removeAttributes(ArgPos, FracToKeep, Mt, Rng);
+  }
+
+  if (changed)
+    return PreservedAnalyses::none();
   return PreservedAnalyses::all();
 }
 
